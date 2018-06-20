66d666fb5490c2938b9e79437b6056ecf729f5e7
parisgirl16 <parisgirl16@sbcglobal.net>
2018-06-20 14:35:36 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,5 +1,7 @@
-def invitation
+def invitation(guest_name,party_name,date,time,rsvp,host_name)
 puts "Dear #{guest_name}"
 puts "You are cordially invited to the Best #{party_name} Ever on #{date} at #{time}. Please RSVP no later than #{rsvp}."
 puts "Sincerely,"
 puts "#{host_name}"
+end 
+invitation("Hermione",)
